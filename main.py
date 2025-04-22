4d08e3ccc3e95426a24a69fe1bd1bfbfe23538b6
Tharindu De Costa <mtndecosta@gmail.com>
2025-04-23 00:03:54 +0530

    create folders

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
+
+UPLOAD_FOLDER = "uploads"
+PROCESSED_FOLDER = "processed_frames"
+
+os.makedirs(PROCESSED_FOLDER, exist_ok=True)
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 @app.route('/home')
 def home():
