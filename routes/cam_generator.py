ebb048f4e5cd359bc133abc8f00ebb35311b5eb0
Tharindu De Costa <mtndecosta@gmail.com>
2025-04-23 23:00:07 +0530

    cam gen fix

--- routes/cam_generator.py
+++ routes/cam_generator.py
@@ -10,30 +10,33 @@ UPLOAD_FOLDER = "uploads"
 @cam_blueprint.route("/api/generateCAM", methods=["POST"])
 def generate_image():
     start_time = time.time()
+    print("Request init 1")
 
+    # Check for video file
     if "video" not in request.files or request.files["video"].filename == "":
         return jsonify({"error": "No video file provided"}), 400
     
+    # Get number of frames
     num_frames = request.form.get("num_frames")
-
-    if not num_frames or request.files["num_frames"] == "":
-        num_frames = 5
-    else:
+    try:
         num_frames = int(num_frames)
+    except (TypeError, ValueError):
+        num_frames = 5 
 
     user_email = request.form.get("email")
-
-    if not user_email or request.files["email"] == "":
+    if not user_email:
         return jsonify({"error": "Email is required"}), 400
 
     os.makedirs(UPLOAD_FOLDER, exist_ok=True)
     video = request.files["video"]
     video_path = os.path.join(UPLOAD_FOLDER, f"temp_video_{video.filename}")
     video.save(video_path)
-    saved_images = generate_cam(video_path,user_email,num_frames)
+
+    saved_images = generate_cam(video_path, user_email, num_frames)
     os.remove(video_path)
+
     end_time = time.time()
-    total_time = end_time - start_time
     return jsonify({
         "processed_images": saved_images,
-        "total_time":round(total_time, 2)}), 200
+        "total_time": round(end_time - start_time, 2)
+    }), 200
